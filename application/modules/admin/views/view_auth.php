f5598faa3994d9a58b1c18d74b3eaf0fa11983c7
5yarif <syarrief@gmail.com>
2018-01-16 08:59:18 +0700

    Update Modul User Management (Create, Read, Update) - User Group (belum()

--- application/modules/admin/views/view_auth.php
+++ application/modules/admin/views/view_auth.php
@@ -13,11 +13,8 @@
 			<div class="page-header">
 				<h1>User Management</h1>
 			</div>
-			<div class="page-content">
-				<h1><?php echo lang('index_heading');?></h1>
-				<p><?php echo lang('index_subheading');?></p>
-				
-				<table cellpadding=0 cellspacing=10>
+			<div class="page-content">				
+				<table class="table table-bordered table-striped" id="mytable">
 					<tr>
 						<th><?php echo lang('index_fname_th');?></th>
 						<th><?php echo lang('index_lname_th');?></th>
@@ -33,16 +30,16 @@
 							<td><?php echo htmlspecialchars($user->email,ENT_QUOTES,'UTF-8');?></td>
 							<td>
 								<?php foreach ($user->groups as $group):?>
-									<?php echo anchor("auth/edit_group/".$group->id, htmlspecialchars($group->name,ENT_QUOTES,'UTF-8')) ;?><br />
+									<?php echo anchor("admin/auth/edit_group/".$group->id, htmlspecialchars($group->name,ENT_QUOTES,'UTF-8')) ;?><br />
 								<?php endforeach?>
 							</td>
-							<td><?php echo ($user->active) ? anchor("auth/deactivate/".$user->id, lang('index_active_link')) : anchor("auth/activate/". $user->id, lang('index_inactive_link'));?></td>
-							<td><?php echo anchor("auth/edit_user/".$user->id, 'Edit') ;?></td>
+							<td><?php echo ($user->active) ? anchor("admin/auth/deactivate/".$user->id, lang('index_active_link')) : anchor("admin/auth/activate/". $user->id, lang('index_inactive_link'));?></td>
+							<td><?php echo anchor("admin/auth/edit_user/".$user->id, 'Edit') ;?></td>
 						</tr>
 					<?php endforeach;?>
 				</table>
 
-				<p><?php echo anchor('auth/create_user', lang('index_create_user_link'))?> | <?php echo anchor('auth/create_group', lang('index_create_group_link'))?></p>			
+				<p><?php echo anchor('admin/auth/create_user', lang('index_create_user_link'))?> | <?php echo anchor('admin/auth/create_group', lang('index_create_group_link'))?></p>			
 			</div>
 		</div>
 	</div>
